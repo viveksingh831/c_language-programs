451e07b9740beba12d61ae52fd16d7c46eae0dc7
vivek singh rajput <vivekmn831@gmail.com>
2026-06-10 08:59:17 +0530

    change some line

--- star2.c
+++ star2.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main(){
-    int i,j,p=1;
+    int i,j,p=65;
     for(i=1;i<=5;i++){
         for(j=1;j<=5;j++){
             if(j<=i){
                 
-                printf("%d",p);
+                printf("%c",p);
                
             }
             else{
